b39eab64ccbbf51d1c1644af4c41c336594ab5dc
kangmina <kmn7890>
2018-01-11 00:00:54 +0900

    views respond 4 types

--- django2018/dojo/urls.py
+++ django2018/dojo/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^sum/(?P<x>\d+)/$', views.mysum),
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
-
+    url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
+    url(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)$', views.hellogongyoo),
+    url(r'^list1/$', views.postlist1),
+    url(r'^list2/$', views.postlist2),
+    url(r'^list3/$', views.postlist3),
+    url(r'^excel/$', views.exceldownload),
 ]
--- django2018/dojo/views.py
+++ django2018/dojo/views.py
@@ -1,7 +1,46 @@
+import os
+from django.conf import settings
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
-def mysum(request, x, y=0, z=0):
+def mysum(request, numbers):
+    # numbers = "1/2/12/123/1234/1231231"
     # request : HttpRequest의 인스턴스
-    return HttpResponse(int(x) + int(y) + int(z))
+    result = sum(map(lambda s: int(s or 0), numbers.split('/')))
+    return HttpResponse(result)
+
+def hellogongyoo(request, name, age):
+    return HttpResponse('Hello {} {} age year old'.format(age, name))
+
+
+def postlist1(request):
+    name = "공유"
+    return HttpResponse('''
+    <h1>Function Based View Responded by HTML Form</h1>
+    <p> {name} </p>
+    <p> Hello, This is written in HTML code format </p>
+    '''.format(name=name))
+
+
+def postlist2(request):
+    name="공유"
+    return render(request, 'dojo/post_list.html', {'name':name})
+
+
+#json 형식으로 응답하기
+def postlist3(request):
+    return JsonResponse({
+        'message': '안녕 파이썬&장고',
+        'items':['python','django','celery','azure','aws']
+    },json_dumps_params={'ensure_ascii':False})
+
+
+def exceldownload(request):
+    # filepath = '/Users/kangmina/djproject/django2018/커리큘럼ver2.xlsx'
+    filepath = os.path.join(settings.BASE_DIR, '커리큘럼ver2.xlsx')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') #text/html
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
